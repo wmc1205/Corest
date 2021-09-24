5f0b37ed33d402cc8dd17950cb3cca3665053882
우민철 <woomincheol@umincheol-ui-MacBookPro.local>
2021-09-24 15:15:43 +0900

    UpdateForm, field disabled 구현

--- accountapp/views.py
+++ accountapp/views.py
@@ -5,8 +5,9 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, UpdateView
 
+from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
 
 
@@ -38,3 +39,10 @@ class AccountDetailView(DetailView):
     model = User
     context_object_name = 'targetUser'
     template_name = 'accountapp/detail.html'
+
+class AccountUpdateView(UpdateView):
+    model = User
+    form_class = AccountUpdateForm
+    context_object_name = 'targetUser'
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/update.html'
