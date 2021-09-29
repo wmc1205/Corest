ce0d4cc00669ffe8dda4c680b3e2fbe22dd31543
우민철 <woomincheol@umincheol-ui-MacBookPro.local>
2021-09-29 10:36:31 +0900

    success url 및 debuging

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView
 
@@ -14,7 +14,7 @@ class ProfileCreateView(CreateView):
     model = Profile
     form_class = ProfileCreationForm
     context_object_name = 'targetProfile'
-    success_url = reverse_lazy('accountapp:hello_world')
+
     template_name = 'profileapp/create.html'
 
     def form_valid(self, form):
@@ -22,6 +22,10 @@ class ProfileCreateView(CreateView):
         temp_profile.user = self.request.user
         temp_profile.save()
         return super().form_valid(form)
+    #원하는 url의 data를 동적으로 전송해주기 위한 함수
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})
 
 @method_decorator(profile_ownership_required, 'get')
 @method_decorator(profile_ownership_required, 'post')
@@ -29,5 +33,8 @@ class ProfileUpdateView(UpdateView):
     model = Profile
     form_class = ProfileCreationForm
     context_object_name = 'targetProfile'
-    success_url = reverse_lazy('accountapp:hello_world')
+
     template_name = 'profileapp/update.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})
