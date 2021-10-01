5a01639bacf244b160a2b3b49c86d2c12be88da6
우민철 <woomincheol@umincheol-ui-MacBookPro.local>
2021-10-01 16:15:55 +0900

    commentapp 시각화 및 deleteview 구현

--- commentapp/decorators.py
+++ commentapp/decorators.py
@@ -4,10 +4,13 @@ from django.http import HttpResponseForbidden
 from articleapp.models import Article
 
 #고유한 pk값에 해당하는 Article의 객체를 얻어 article 변수에 저장한다.
-def article_ownership_required(func):
+from commentapp.models import Comment
+
+
+def comment_ownership_required(func):
     def decorated(request, *args, **kwargs):
-        article = Article.objects.get(pk=kwargs['pk'])
-        if not article.writer == request.user:
+        comment = Comment.objects.get(pk=kwargs['pk'])
+        if not comment.writer == request.user:
             return HttpResponseForbidden()
         return func(request,*args,**kwargs)
 
