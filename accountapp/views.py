aea676662eb49cd7318a63e4e168caf1a710def6
우민철 <woomincheol@umincheol-ui-MacBookPro.local>
2021-11-07 00:23:20 +0900

    requirements.txt 수정

--- accountapp/views.py
+++ accountapp/views.py
@@ -21,8 +21,12 @@ has_ownership = [login_required,account_ownership_required]
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+
     template_name = 'accountapp/create.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.pk})
+
 
 class AccountDetailView(DetailView, MultipleObjectMixin):
     model = User
